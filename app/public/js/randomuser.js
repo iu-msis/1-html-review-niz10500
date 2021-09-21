ad0980b0b9649821b967867ded82b66a00071c95
niz10500 <niz@iu.edu>
2021-09-21 19:41:30 -0400

    Update Reload
    Update Reload Button

--- app/public/js/randomuser.js
+++ app/public/js/randomuser.js
@@ -13,19 +13,23 @@ const User = {
       
     },
 
-    created(){
-
-      fetch('https://randomuser.me/api/')   
-      .then(response =>response.json())
-      .then((parsedJson) =>{
-        console.log(parsedJson);
-          this.person = parsedJson.results[0]
-
-      })
-      .catch( err => {
-        console.error(err)
-      })
-
+    methods: {
+      fetchUserData() {
+        fetch('https://randomuser.me/api/')   
+        .then(response =>response.json())
+        .then((parsedJson) =>{
+          console.log(parsedJson);
+            this.person = parsedJson.results[0]
+  
+        })
+        .catch( err => {
+          console.error(err)
+        })
+      }
+  },
+  created() {
+      this.fetchUserData();
   }
+
 }
 Vue.createApp(User).mount('#userApp');
