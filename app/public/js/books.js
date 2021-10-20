3560271a4becfa4c31fb60e2660113188790984b
niz10500 <niz@iu.edu>
2021-10-20 08:33:02 -0400

    Post(button)
    Post

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -2,12 +2,26 @@
 const Books = {
     data() {
       return {
-          books: []
-        
+          books: [],
+          bookForm: {},
+          selectedBook: null,
+    
         }
     },
     computed: {},
     methods: {
+        prettyDollar(n) {
+            const d = new Intl.NumberFormat("en-US").format(n);
+            return "$ " + d;
+        },
+        selectBook(s) {
+            if (s == this.selectedBook) {
+                return;
+            }
+            this.selectedBook = s;
+            this.books = [];
+            this.fetchBookData(this.selectedBook);
+        },
         fetchBookData() {
             fetch('/api/book/')
             .then( response => response.json() )
@@ -20,11 +34,33 @@ const Books = {
             })
         },
 
-    },
-    created() {
-        return this.fetchBookData();
-     
-    }
+
+    postNewBook(evt) {     
+        console.log("Posting:", this.bookForm);
+        alert("Posting!");
+
+        fetch('api/book/create.php', {
+            method:'POST',
+            body: JSON.stringify(this.bookForm),
+            headers: {
+              "Content-Type": "application/json; charset=utf-8"
+            }
+          })
+          .then( response => response.json() )
+          .then( json => {
+            console.log("Returned from post:", json);
+            // TODO: test a result was returned!
+            this.books = json;
+            
+            // reset the form  （clean the form)
+            this.bookForm = {};
+          });
+      }
+  },
+   
+  created() {
+    return this.fetchBookData();
+ }
 }
-  
+
 Vue.createApp(Books).mount('#bookApp');
